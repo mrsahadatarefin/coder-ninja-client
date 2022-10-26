7efffd290188a8c6cdd49d36645850352eae7ffc
mrsahadatarefin <sahadatarifin806@gmail.com>
2022-10-26 21:17:35 +0600

    few

--- src/Login/LOgin.js
+++ src/Login/LOgin.js
@@ -6,31 +6,26 @@ import Form from "react-bootstrap/Form";
 import { useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../Contexts/Authprovider/AuthProvider";
 
-
-
 const LOgin = () => {
   const { singIN } = useContext(AuthContext);
-  const navigate  =useNavigate()
-const location=useLocation();
-const from =location.state?.from?.pathname||"/";
-
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email,password)
-    singIN(email,password)
-    .then (result =>{
-      const user = result.user;
-      console.log(user)
-      form.reset()
-      navigate(from,{replace:true});
-    })
-    .catch (error =>console.error(error))
-      
-    
+    console.log(email, password);
+    singIN(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        navigate(from, { replace: true });
+      })
+      .catch((error) => console.error(error));
   };
   return (
     <Form onSubmit={handleSubmit} style={{ margin: "auto", width: "30vw" }}>
@@ -46,9 +41,7 @@ const from =location.state?.from?.pathname||"/";
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" name="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
