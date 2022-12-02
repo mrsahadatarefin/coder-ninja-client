51097c5be10e13fdf07b42a12038b6b4bdb3f78b
mrsahadatarefin <sahadatarifin806@gmail.com>
2022-12-02 21:50:42 +0600

    project done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import img from "../Home/react.png"
+
 const Home = () => {
     return (
         <div>
-            <h1>Home
+            <h1 style={{textAlign:"center" ,padding:"10px"}}>Learn Something Good</h1>
+           <img  style={{ height:"70vh", width:"100vw",padding:"10px"}} src={img} alt="" srcset="" /> 
                 
-            </h1>
+            
             
         </div>
     );
